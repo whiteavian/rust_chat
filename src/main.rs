21bf3dbbcb071f461f7a4eedcefa90e907f0cffc
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-30 18:01:00 -0400

    REF: try out a much simple synchronous example using std crates.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
-extern crate futures;
-extern crate tokio_core;
-extern crate tokio_io;
+//extern crate futures;
+//extern crate tokio_core;
+//extern crate tokio_io;
+//
+//use futures::{Future, Stream};
+//use tokio_core::net::TcpListener;
+//use tokio_core::reactor::Core;
+//use tokio_io::io::{read_to_end, write_all};
+//use tokio_io::AsyncRead;
+//
+//
+//// This is more or less identical to the example provided by:
+//// https://tokio-rs.github.io/tokio-core/tokio_core/index.html
+//fn main() {
+//
+//    let mut core = Core::new().unwrap();
+//    let handle = core.handle();
+//    let address = "127.0.0.1:8001".parse().unwrap();
+//    let server_socket = TcpListener::bind(&address, &handle).unwrap();
+//
+//    let server = server_socket.incoming().for_each(|(sock, _)| {
+////        let (reader, writer) = sock.split();
+////        let result = sock.read_to_string();
+//
+//        Ok(())
+//    });
+//
+//    core.run(server).unwrap();
+//}
 
-use futures::{Future, Stream};
-use tokio_core::net::TcpListener;
-use tokio_core::reactor::Core;
-use tokio_io::io::copy;
-use tokio_io::AsyncRead;
 
+// Simpler example:
+// https://medium.com/adventures-in-rust/moving-to-tcpstream-bye-tokio-5a1488f337f6
+use std::net::{TcpStream, TcpListener};
+use std::io::Write;
 
-// This is more or less identical to the example provided by:
-// https://tokio-rs.github.io/tokio-core/tokio_core/index.html
 fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8001").unwrap();
 
-    let mut event_loop = Core::new().unwrap();
-    let handle = event_loop.handle();
-//    let address = "127.0.0.1:8001".parse::<SocketAddr>().unwrap();
-    let address = "127.0.0.1:8001".parse().unwrap();
-    let server_socket = TcpListener::bind(&address, &handle).unwrap();
-
-    let server = server_socket.incoming().for_each(|(sock, _)| {
-        let (reader, writer) = sock.split();
-
-        let bytes_copied = copy(reader, writer);
-
-        let handle_conn = bytes_copied.map(|amt| {
-            println!("wrote {:?} bytes", amt)
-        }).map_err(|err| {
-            println!("IO error {:?}", err)
-        });
-
-        handle.spawn(handle_conn);
-
-        Ok(())
-    });
-
-    event_loop.run(server).unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let response = b"Hello World!\n";
+                stream.write(response).expect("Response failed.");
+            }
+            Err(e) => {
+                println!("Unable to connect: {}.", e);
+            }
+        }
+    }
 }
